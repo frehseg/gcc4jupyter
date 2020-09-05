3289880750a3b7dfe8bfabeb90b33b686112ce8f
Goran Frehse <goranf@gmail.com>
2020-09-05 12:17:18 +0200

    changes from nvcc to gcc

--- gcc_plugin.py
+++ gcc_plugin.py
@@ -6,22 +6,22 @@ import tempfile
 import subprocess
 import IPython.core.magic as ipym
 
-compiler = '/usr/local/cuda/bin/nvcc'
-compiler_options = '-I /usr/local/cuda/samples/common/inc/ -L/usr/local/cuda/include -lcublas -lcusolver'
-ext = '.cu'
+compiler = '/usr/bin/gcc'
+compiler_options = '-lm'
+ext = '.c'
 
 
 def get_argparser():
-    parser = argparse.ArgumentParser(description='NVCCPlugin params')
+    parser = argparse.ArgumentParser(description='GCCPlugin params')
     parser.add_argument("-t", "--timeit", action='store_true',
                         help='flag to return timeit result instead of stdout')
     return parser
 
 @ipym.magics_class
-class NVCCPlugin(ipym.Magics):
+class GCCPlugin(ipym.Magics):
 
     def __init__(self, shell):
-        super(NVCCPlugin, self).__init__(shell)
+        super(GCCPlugin, self).__init__(shell)
         self.argparser = get_argparser()
 
     def run(self, file_path, timeit=False):
@@ -32,7 +32,7 @@ class NVCCPlugin(ipym.Magics):
             return subprocess.check_output([file_path + ".out"], stderr=subprocess.STDOUT).decode("utf-8")
 
     @ipym.cell_magic
-    def cu(self, line, cell):
+    def c(self, line, cell):
         try:
             args = self.argparser.parse_args(line.split())
         except SystemExit as e:
@@ -44,11 +44,11 @@ class NVCCPlugin(ipym.Magics):
             with open(file_path + ext, "w") as f:
                 f.write(cell)
             try:
-                subprocess.check_output([compiler, '-I/usr/local/cuda/samples/common/inc/', '-L/usr/local/cuda/include', '-lcublas', '-lcusolver', file_path + ext, "-o", file_path + ".out"], stderr=subprocess.STDOUT)
+                subprocess.check_output([compiler, '-lm', file_path + ext, "-o", file_path + ".out"], stderr=subprocess.STDOUT)
                 print(self.run(file_path, timeit=args.timeit))
             except subprocess.CalledProcessError as e:
                 print(e.output.decode("utf8"))
 
 def load_ipython_extension(ip):
-    nvcc_plugin = NVCCPlugin(ip)
-    ip.register_magics(nvcc_plugin)
+    gcc_plugin = GCCPlugin(ip)
+    ip.register_magics(gcc_plugin)
