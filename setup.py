e9265708c63463f15cb5572cb649b351e051833a
frehseg <30627181+frehseg@users.noreply.github.com>
2020-09-13 10:58:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.2',
     author='Goran Frehse',
     author_email='goranf@gmail.com',
-    py_modules=['gcc_plugin'],
+    py_modules=['gcc_plugin','add_cpp_magic'],
     url='htpps://github.com/frehseg/gcc4jupyter',
     license='LICENSE',
     description='Jupyter notebook plugin to run C/C++ code',
