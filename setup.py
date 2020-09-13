164ce821aabe3c19b3e9eec8e54962614ad01415
frehseg <30627181+frehseg@users.noreply.github.com>
2020-09-13 10:57:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='GCCPlugin',
-    version='0.0.1',
+    version='0.0.2',
     author='Goran Frehse',
     author_email='goranf@gmail.com',
     py_modules=['gcc_plugin'],
