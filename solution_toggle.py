48983aacad404ae9a79c7153f84d846e0a36b7c2
Goran Frehse <goranf@gmail.com>
2020-09-13 20:29:50 +0200

    bugfix

--- solution_toggle.py
+++ solution_toggle.py
@@ -1,26 +1,26 @@
-def load_ipython_extension(ipython):
 from IPython.display import HTML
-HTML('''<script>
-code_show=true; 
-function solution_toggle() {
- if (code_show){
- $('.cm-comment:contains(@solution)').closest('div.input').hide();
- } else {
- $('.cm-comment:contains(@solution)').closest('div.input').show();
- }
- code_show = !code_show
-} 
-$( document ).ready(solution_toggle);
-if($(IPython.toolbar.selector.concat(' > #toggle solution')).length == 0){
-  IPython.toolbar.add_buttons_group([
-        {
-             'label'   : 'toggle solution',
-             'icon'    : 'fa fa-angle-double-down',
-             'callback': function(){
-                 $( document ).ready(solution_toggle);
-             }
-        }
-    ], 'toggle-solution');
-}
-</script>''')
+def load_ipython_extension(ipython):
+  HTML('''<script>
+  code_show=true;
+  function solution_toggle() {
+   if (code_show){
+   $('.cm-comment:contains(@solution)').closest('div.input').hide();
+   } else {
+   $('.cm-comment:contains(@solution)').closest('div.input').show();
+   }
+   code_show = !code_show
+  }
+  $( document ).ready(solution_toggle);
+  if($(IPython.toolbar.selector.concat(' > #toggle solution')).length == 0){
+    IPython.toolbar.add_buttons_group([
+          {
+               'label'   : 'toggle solution',
+               'icon'    : 'fa fa-angle-double-down',
+               'callback': function(){
+                   $( document ).ready(solution_toggle);
+               }
+          }
+      ], 'toggle-solution');
+  }
+  </script>''')
 
